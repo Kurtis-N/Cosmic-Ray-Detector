f406fd2503f84c4d6b676526dd19702cda206044
Kurtis Niedling <kc.niedling@gmail.com>
2024-05-27 18:03:43 -0600

    Add files via upload
    Adding cosmic ray detector class

---
+++ CosmicRayDetector.java
@@ -0,0 +1,11 @@
+public class CosmicRayDetector {
+        public static void main(String[] args) {
+                detectCosmicRays();
+        }
+
+        private static void detectCosmicRays() {
+                boolean rayDetected = false;
+                while(!rayDetected) {}
+                System.out.println("Cosmic ray detected.");
+        }
+}
